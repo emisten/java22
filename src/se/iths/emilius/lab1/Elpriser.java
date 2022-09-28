3b66931003b967611ebcde36e80521b6daa510f7
iths22 <emil.stenhagen@gmail.com>
2022-09-28 07:32:44 +0200

    Lab1

--- src/se/iths/emilius/lab1/Elpriser.java
+++ src/se/iths/emilius/lab1/Elpriser.java
@@ -30,13 +30,18 @@ public class Elpriser {
                     break;
 
                 case "3":
-                    System.out.println("Billigast till dyrast pris: ");
                     sortPrices();
                     break;
 
                 case "4":
-                    System.out.println("Bästa laddningstid 4 timmar isträck: ");
+                    cheapestHours();
                     break;
+
+
+                case "5":
+                    visualizePrices();
+                    break;
+
 
                 case "E":
                     run = false;
@@ -53,13 +58,13 @@ public class Elpriser {
     private static void printMenu() {
         System.out.println();
         System.out.println();
-        System.out.println();
         System.out.println("Elpriser");
         System.out.println("========");
         System.out.println("1. Inmatning");
         System.out.println("2. Min, max och Medel");
         System.out.println("3. Sortera");
         System.out.println("4. Bästa Laddningstid");
+        System.out.println("5. Visualisering");
         System.out.println("e. Avsluta");
     }
 
@@ -86,8 +91,7 @@ public class Elpriser {
             sum += currentPrice.price;
 
         }
-        int average = sum/24;
-
+        int average = sum / 24;
 
 
         System.out.printf("Lägsta pris kl: %02d-%02d %d öre%n", hourlyPrices[0].hour, hourlyPrices[0].hour + 1, hourlyPrices[0].price);
@@ -111,7 +115,88 @@ public class Elpriser {
 
     private static void cheapestHours() {
 
+        Arrays.sort(hourlyPrices, Comparator.comparingInt(o -> o.hour));
+        int cheapestHour = 0;
+        int cheapestPrice = Integer.MAX_VALUE;
+
+
+        for (int i = 0; i < 21; i++) {
+            HourlyPrice currentHour = hourlyPrices[i];
+            HourlyPrice secondHour = hourlyPrices[i + 1];
+            HourlyPrice thirdHour = hourlyPrices[i + 2];
+            HourlyPrice fourthHour = hourlyPrices[i + 3];
+
+            int combinedPrice = currentHour.price + secondHour.price + thirdHour.price + fourthHour.price;
+            if (combinedPrice < cheapestPrice) {
+                cheapestHour = currentHour.hour;
+                cheapestPrice = combinedPrice;
+            }
+        }
+
+        System.out.printf("Börja ladda kl: %02d för det bästa laddningspriset. Medelpriset för de fyra bästa timmarna i sträck var : %d öre", cheapestHour, cheapestPrice / 4);
+
+
     }
 
+
+    private static void visualizePrices() {
+
+        Arrays.sort(hourlyPrices, Comparator.comparingInt(o -> o.price));
+
+
+        int height = 6;
+        int maxValue = hourlyPrices[23].price;
+        int lowestValue = hourlyPrices[0].price;
+        int increment = maxValue / height;
+        int width = String.valueOf(maxValue).length();
+        StringBuilder widthSb = new StringBuilder();
+        for (int j = 0; j < width; j++) {
+            widthSb.append(" ");
+        }
+
+
+        Arrays.sort(hourlyPrices, Comparator.comparingInt(o -> o.hour));
+
+
+        for (int i = 0; i < height; i++) {
+            int threshold = maxValue - (i * increment);
+            StringBuilder sb = new StringBuilder();
+
+
+            if (i == 0) {
+                sb.append(maxValue);
+            } else if (i == height - 1) {
+                int lowestValueWidth = String.valueOf(lowestValue).length();
+                int spacesToAdd = width - lowestValueWidth;
+                for (int j = 0; j < spacesToAdd; j++) {
+                    sb.append(" ");
+                }
+                sb.append(lowestValue);
+            } else {
+                sb.append(widthSb);
+            }
+            sb.append(" | ");
+            for (int j = 0; j < 24; j++) {
+                int currentValue = hourlyPrices[j].price;
+                if (currentValue >= threshold) {
+                    sb.append("x");
+                } else {
+                    sb.append(" ");
+
+                }
+                sb.append("  ");
+
+            }
+
+            System.out.println(sb);
+
+
+        }
+
+        System.out.println(widthSb + " | -----------------------------------------------------------------------");
+        System.out.println(widthSb + " | 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23");
+
+
+    }
 
 }
